cae7015a0dea827be4fc4fede7cf3da4807394dd
Marco <marco_bonetti@uol.com.br>
2016-04-04 21:55:39 -0300

    Estado final do site

--- Gemfile
+++ Gemfile
@@ -11,6 +11,12 @@ group :development do
 	gem 'capybara'
 end
 
+group :test do
+  gem 'minitest-reporters', '1.0.5'
+  gem 'mini_backtrace',     '0.1.3'
+  gem 'guard-minitest',     '2.3.1'
+end
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,27 +1,42 @@
 require "spec_helper"
 
 describe "Static Pages", :type => :feature do
-	
+
 	describe "Home Page", :type => :feature do
+		it "deveria ter em h1 Sample App" do
+		visit '/static_pages/home'
+		page.should have_selector("h1",:text => "Sample App")
+		end
+		
 		it "deveria ter o conteudo Sample App" do
-		# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
 		visit '/static_pages/home' 
-		page.should have_content ( "Sample App" )
+		page.should have_selector("title",:text=>"Home")
 		end
 	 end
     
 	describe "Help Page", :type => :feature do
 		it "deveria ter o conteúdo Help" do
 		visit '/static_pages/help'
-		page.should have_content ( "Help" )
+		page.should have_selector("h1",:text=>"Help")
+		end
+		
+		it "deveria ter o título Help" do
+		visit '/static_pages/help'
+		page.should have_selector("title",:text=>"Help | Ruby on Rails Tutorial Sample App")
 		end
 	end
 		
 	describe "About Page", :type => :feature do
+	
 		it "deveria ter o conteúdo About Us" do
 		visit '/static_pages/about'
-		page.should have_content ( "About Us" )
+		page.should have_selector("h1",:text=>"About Us" )
 		end
+		it "deveria ter o título About Us" do
+		visit '/static_pages/about'
+		page.should have_selector("title",:text=>"About | Ruby on Rails Tutorial Sample App")
+		end
+		
 	end
 	
 end
