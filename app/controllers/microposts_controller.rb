7b8c870642aaedd5f6218404ac92751d5947f382
Marco Bonetti <marco_bonetti@uol.com.br>
2017-02-09 01:17:27 -0200

    ateh micropost

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,34 +1,23 @@
-class UsersController < ApplicationController
+class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
 
-
-  def index
-
-  end
-  
-  def show
-
-#	    debugger
-  end
-
-  def new
-
-  end
-  
   def create
-
+      @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost criado!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
-  
-  def edit
-end
 
-  def update
-
-  end
-  
   def destroy
-
   end
+  
+    private
 
-
-
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+	
 end
